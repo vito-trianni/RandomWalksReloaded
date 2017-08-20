04e42c9200bdc9abbaeeec39ba2d487017dd2d15
katanachan <shwarya.unnikrishnan@gmail.com>
2017-08-20 08:01:17 +0530

    V2, computing extrapolated first passage of time with each agent in swarm. Also added functionality for validating experimental statistics at different time limits

--- multi-agent-simulations/analysis/visualize.py
+++ multi-agent-simulations/analysis/visualize.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 This is a script to automate visualising data in a batch.
 1. First files in the specified directory are parsed. Add your extension to the tail argument in subtract to feed correct data.
@@ -19,7 +18,6 @@ import argparse
 import glob
 import os
 from itertools import groupby
-import operator
 import scipy.stats as st
 import scipy.special as sc
 import copy
@@ -32,37 +30,52 @@ class Weib(st.rv_continuous):
         st.rv_continuous.__init__(self,a=lower_bound)
     def _cdf(self,x,alpha,gamma):
         return 1 - np.exp(-np.power(x/alpha,gamma))
-
+def expanded_stats(data):
+    time_list=[]
+    for i in range(data.shape[0]):
+        censored=np.sum(np.isnan(data[i,:]))
+        if censored==data.shape[1]:
+            time_list.append(np.nan)
+        else:
+            dataset=data[i,:]
+            dataset=dataset[np.argsort(dataset)]
+            dataset=dataset[:-censored]
+            _,gamma,_,alpha=st.exponweib.fit(dataset,floc=0,f0=1)
+            time_list.append(sc.gamma(1+(1/gamma))*alpha)
+            
+    return np.asarray(time_list).reshape(1,-1)[0]    
+        
+    
+        
 def stats(p,truncated=True):
     convergence_array = np.array(p[:,0])
     convergence_time_array = np.ma.array(p[:,1], mask=np.logical_not(convergence_array))
-    censored=np.sum(np.logical_not(convergence_array))
-    print ("Censored Vallues",censored)
+    censored_conv=np.sum(np.logical_not(convergence_array))
+    print ("Censored Vallues",censored_conv)
     conv_time_array = np.ma.array(p[:,2], mask=np.logical_not(convergence_array))
-    efficiency_array = np.ma.array(p[:,3], mask=np.logical_not(convergence_array))
-    average_total_time_array = np.ma.array(p[:,4], mask=np.logical_not(convergence_array))
-    total_visits_array = np.ma.array(p[:,5], mask=np.logical_not(convergence_array))
-    percentage_tot_agents_with_info_array = np.ma.array(p[:,6], mask=np.logical_not(convergence_array))
+    visits_ratio_array = np.ma.array(p[:,3], mask=np.logical_not(convergence_array))
+    percentage_tot_agents_with_info_array = np.ma.array(p[:,4], mask=np.logical_not(convergence_array))    
+    first_passage_time_array=np.ma.array(expanded_stats(p[:,5:]),mask=np.logical_not(convergence_array))
+    
     if truncated==True:
         convergence_time_array.mask=np.ma.nomask
         conv_time_array.mask=np.ma.nomask
-        efficiency_array.mask=np.ma.nomask
-        average_total_time_array.mask=np.ma.nomask
-        total_visits_array.mask=np.ma.nomask
+        visits_ratio_array.mask=np.ma.nomask
         percentage_tot_agents_with_info_array.mask=np.ma.nomask
+        first_passage_time_array.mask=np.ma.nomask
+    censored_pass=np.sum(np.isnan(first_passage_time_array))
     dataset = np.column_stack((convergence_array,
-        convergence_time_array,
-        conv_time_array,
-        efficiency_array,
-        average_total_time_array,
-        total_visits_array,
-        percentage_tot_agents_with_info_array))
+                               convergence_time_array,
+                               conv_time_array,
+                               visits_ratio_array,
+                               percentage_tot_agents_with_info_array,
+                               first_passage_time_array))
         
-    if censored==0 or truncated==False:
-        head = [np.mean(convergence_array.astype(int)), np.mean(convergence_time_array.compressed()), np.mean(conv_time_array.compressed()) , np.mean(p[:,3]), np.mean(p[:,4]), np.mean(p[:,5]), np.mean(p[:,6]) ]
+    if (censored_conv==0 and censored_pass == 0) or truncated==False:
+        head = [np.mean(convergence_array.astype(int)), np.mean(convergence_time_array.compressed()), np.mean(conv_time_array.compressed()) , np.mean(p[:,3]), np.mean(p[:,4]),np.mean(p[:,5])]
     else:
-        dataset=dataset[np.argsort(dataset[:,1])]
-        n_est=np.asarray(range(0,conv_time_array.compressed().size-censored))[::-1] + float(censored)
+        data=dataset[np.argsort(dataset[:,1])]
+        n_est=np.asarray(range(0,conv_time_array.compressed().size-censored_conv))[::-1] + float(censored_conv)
         RT_sync=[]
         for i in range(n_est.size):
             if len(RT_sync)==0:
@@ -70,27 +83,44 @@ def stats(p,truncated=True):
             else:
                 RT_sync.append(RT_sync[-1]*((n_est[i]-1)/n_est[i]))
         F=1-np.asarray(RT_sync).reshape(-1,1)
-        compute=np.concatenate((dataset[:-censored,1:3],F),1)
-        m,gamma,c,alpha=st.exponweib.fit(compute[:,0],floc=0,f0=1)
-        my=Weib()
-        #print alpha,gamma,compute[:,0].shape
-        ys=my.cdf(compute[:,0],alpha,gamma)
-        #error= np.mean(np.power(ys-F,2))
-        #error2=np.mean(np.power(y2-F,2))
-        #print "ERROR", error-error2
+        if censored_conv !=0:
+            compute=np.concatenate((data[:-censored_conv,1:3],F),1)
+        try:
+            m,gamma,c,alpha=st.exponweib.fit(compute[:,0],floc=0,f0=1)
+            my=Weib()
+            #print alpha,gamma,compute[:,0].shape
+            ys=my.cdf(compute[:,0],alpha,gamma)
+            #error= np.mean(np.power(ys-F,2))
+            #error2=np.mean(np.power(y2-F,2))
+            #print "ERROR", error-error2
         
-        plt.plot(compute[:,0],ys,'r',linewidth=5)
-        plt.plot(compute[:,0],F,'b',linewidth=5)
-        #plt.show()
-        # Uncomment top line to view the fit
+            plt.plot(compute[:,0],ys,'r',linewidth=5)
+            plt.plot(compute[:,0],F,'b',linewidth=5)
+            #plt.show()
+            # Uncomment top line to view the fit
 
-        Tsync=(sc.gamma(1+(1/gamma))*alpha)
-        print ("Censored",Tsync)
-        convergence_time_array.mask=np.logical_not(convergence_array)
-        print ("Uncensored", np.mean(convergence_time_array.compressed()))
-        _,gamma,_,alpha=st.exponweib.fit(compute[:,1],floc=0,f0=1)
-        Tsynt=sc.gamma(1+(1/gamma))*alpha
-        head = [np.mean(convergence_array.astype(int)), Tsync, Tsynt, np.mean(p[:,3]), np.mean(p[:,4]), np.mean(p[:,5]), np.mean(p[:,6])]
+            Tsync=(sc.gamma(1+(1/gamma))*alpha)
+            print ("Censored",Tsync)
+            convergence_time_array.mask=np.logical_not(convergence_array)
+            print ("Uncensored", np.mean(convergence_time_array.compressed()))
+            _,gamma,_,alpha=st.exponweib.fit(compute[:,1],floc=0,f0=1)
+            Tsynt=sc.gamma(1+(1/gamma))*alpha
+        except:
+            Tsync=np.nan
+            Tsynt=np.nan
+
+        ## First passage time Statistic
+        try:
+            data=dataset[np.argsort(dataset[:,-1])]
+            data=data[:,-1]
+            if censored_pass!=0:
+                data=data[:-censored_pass]
+            _,gamma,_,alpha=st.exponweib.fit(data,floc=0,f0=1)
+            Tpass=sc.gamma(1+(1/gamma))*alpha
+        except:
+            Tpass=np.nan
+            
+        head = [np.mean(convergence_array.astype(int)), Tsync, Tsynt, np.mean(p[:,3]), np.mean(p[:,4]),Tpass]
     return head
 
 class Variable_Dictionary:
@@ -111,7 +141,7 @@ def plot_design(data,x,y,out,plttype,title,unbounded,rho=None,alpha=None,size=No
 	plttype=str(plttype)
 	title=str(title)
         if unbounded == 0:
-                graph_limit=3.5e3
+                graph_limit=3.5e4
                 title+=" for Bounded"
         else:
                 title+=" for Unbounded"
@@ -160,7 +190,7 @@ def plot_design(data,x,y,out,plttype,title,unbounded,rho=None,alpha=None,size=No
 		bin_y=np.asarray((np.asarray(data[y.name])-float(y.length[0]))/(float(y.length[-1])-float(y.length[-2])))
 		bin_x=np.asarray((np.asarray(data[x.name])-float(x.length[0]))/(float(x.length[-1])-float(x.length[-2])))
                 ## Uncomment this to verify your data
-		data.to_csv('final_data.csv',header=False)
+		#data.to_csv('final_data.csv',header=False)
 		bin_y=len(y.length)-bin_y-1		
 		dummy = np.zeros((len(y.length),len(x.length)))
 
@@ -195,6 +225,7 @@ def main():
     ap.add_argument("-t", "--truncated", help="1 or 0")
     ap.add_argument("-u", "--unbounded", help="0 or 1")
     ap.add_argument("-p", "--path", help="enter path")
+    ap.add_argument("-l", "--timelimit", help="Enter unique filename prefix for time limit") 
     args=vars(ap.parse_args())
     if args.get("truncated") is None:
         truncated=0
@@ -202,33 +233,38 @@ def main():
         truncated=int(args["truncated"])
     unbounded=int(args["unbounded"])
     path=str(args["path"])
+    if args.get("timelimit") is None:
+        prefix='result_'
+    else:
+        prefix=str(args["timelimit"])
     log=[]
 
     for filepath in glob.glob(os.getcwd()+'/'+path+'*.dat'):
     	name_labels=[]
         k=[]
         save=False
-        file_dict=subtract(filepath,os.getcwd()+'/'+path+'result_').split('_')
-        for i in range(len(file_dict)):
-        	name_labels.append(str([''.join(g) for _, g in groupby(file_dict[i], str.isalpha)][0]))
-        	k.append(float([''.join(g) for _, g in groupby(file_dict[i], str.isalpha)][1]))
-        if unbounded == 1 and len(k)==5:
-        	name_labels=name_labels[1:]
-        	k=k[1:]
-        	save = True
-        elif unbounded == 0 and len(k)==4:
-            save = True
-        print k
-        if save:
-            b=np.genfromtxt(filepath)
-            k+=stats(b)
-            log.append(k)
+        if filepath.split('/')[-1].split('_')[0] == prefix:
+            file_dict=subtract(filepath,os.getcwd()+'/'+path+prefix+'_').split('_')
+            for i in range(len(file_dict)):
+                #name_labels.append(str([''.join(g) for _, g in groupby(file_dict[i], str.isalpha)][0]))
+                k.append(float([''.join(g) for _, g in groupby(file_dict[i], str.isalpha)][1]))
+            if unbounded == 1 and len(k)==5:
+        	    #name_labels=name_labels[1:]
+        	    k=k[1:]
+        	    save = True
+            elif unbounded == 0 and len(k)==4:
+                save = True
+            print k
+            if save:
+                b=np.genfromtxt(filepath)
+                k+=stats(b)
+                log.append(k)
     log=pd.DataFrame(log)
     log.columns=["Bias","Levy-Exponent-Alpha","CRW-Exponent-Rho","Population-Size"]+["Convergence_Count","Convergence-Time","Relative Convergence Time",
-        "Efficiency","First Time of Passage","Total Visits","Percentage of Total Agents with Info"]
+                                                                                     "Ratio of Total Visits","Percentage of Total Agents with Info","First Time of Passage"]
 
     plot_design(log,"CRW-Exponent-Rho","Levy-Exponent-Alpha","First Time of Passage","heatmap","Average First Passage Time for all Populations",unbounded)
-    plot_design(log,"CRW-Exponent-Rho","Levy-Exponent-Alpha","Total Visits","heatmap","PhiC ratio of Visited to Total Agents",unbounded)
+    plot_design(log,"CRW-Exponent-Rho","Levy-Exponent-Alpha","Ratio of Total Visits","heatmap","PhiC ratio of Visited to Total Agents",unbounded)
     plot_design(log,"Population-Size","Convergence-Time","CRW-Exponent-Rho","lmplot","Convergence Times for Alpha=1.6",unbounded,alpha=1.6)
     plot_design(log,"Population-Size","Convergence-Time","Levy-Exponent-Alpha","lmplot","Convergence Times for Rho=0.6",unbounded,rho=0.6)
     plot_design(log,"CRW-Exponent-Rho","Levy-Exponent-Alpha","Convergence-Time","heatmap","Total Convergence Time for Population 10",unbounded,size=10)
